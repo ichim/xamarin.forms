7df7cf275003a289cb539f79930ebd5299ef1ba5
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2021-04-27 20:03:27 +0300

    style 6

---
+++ stype6/stype4/stype4/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace stype4
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Resources["dynamic_background"] = this.username.Text == null ? Resources["dynamic_style_error"] : Resources["dynamic_style_ok"];
+        }
+    }
+}
