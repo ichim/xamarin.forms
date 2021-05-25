b72ebb6cb70ddf0c729d81385e46ba58f2823d25
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2021-05-25 16:55:28 +0300

    DataTrigger

---
+++ DataTrigger/DataTrigger/DataTrigger/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataTrigger
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+           // this.box1.Rotation+=5;
+           
+        }
+    }
+}
