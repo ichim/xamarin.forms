0fdca3ad25141ae72d789e1dccaa2910222bbe9c
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2021-06-21 14:18:18 +0300

    DragDropImage

---
+++ DragDropImage/DragDropImage/DragDropImage/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DragDropImage
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
+        {
+            Image image = (sender as Element)?.Parent as Image;
+            image.Opacity = 0.4;
+            e.Data.Properties.Add("Demo", image);
+        }
+
+        private void DropGestureRecognizer_Drop(object sender, DropEventArgs e)
+        {
+            var image = e.Data.Properties["Demo"] as Image;
+            var frame = (sender as Element)?.Parent as Frame;
+            frame.Content = image;
+            Task.Run(async () => {
+                await image.RotateTo(360, 1000);
+            });
+            Task.Run(async () => {
+                await image.ScaleTo(1.4, 400);
+                await image.FadeTo(1, 600);
+            });
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
