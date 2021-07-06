55f4f7320c83d6758e7e3d51e366986f61427b8d
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2021-07-06 11:42:21 +0300

    Notification

---
+++ Notification/Not/Not.Android/AndroidNotification.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.Core.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+[assembly:Dependency(typeof(Not.Droid.AndroidNotification))]
+namespace Not.Droid
+{
+    public class AndroidNotification : ICustomNotification
+    {
+        const string channel_id = "default";
+        const string channel_name = "Default";
+        int notify_index = 0;
+
+        public void send(string title, string message)
+        {
+            NotificationManager manager = (NotificationManager)Android.App.Application.Context.GetSystemService(Android.App.Application.NotificationService);
+            
+                var channelNameJava = new Java.Lang.String(channel_name);
+                var channel = new NotificationChannel(channel_id, channelNameJava, NotificationImportance.High)
+                {
+                    Description = "Channel Description"
+                };
+            
+                manager.CreateNotificationChannel(channel);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(Android.App.Application.Context, channel_id)
+
+                .SetContentTitle(title)
+                .SetContentText(message)
+                .SetLargeIcon(BitmapFactory.DecodeResource(Android.App.Application.Context.Resources, Resource.Drawable.referee))
+                .SetSmallIcon(Resource.Drawable.notify_panel_notification_icon_bg)
+                .SetPriority((int)NotificationPriority.High)
+                .SetVisibility((int)NotificationVisibility.Public)
+                .SetDefaults((int)NotificationDefaults.Sound | (int)NotificationDefaults.Vibrate);
+
+            Notification notification = builder.Build();
+            manager.Notify(notify_index++, notification);
+
+        }
+    }
+}
---
+++ Notification/Not/Not/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Not
+{
+    public partial class MainPage : ContentPage
+    {
+        ICustomNotification notification;
+        public MainPage()
+        {
+            InitializeComponent();
+            notification = DependencyService.Get<ICustomNotification>();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var t = Task.Run(async delegate
+            {
+                await Task.Delay(5000);
+                notification.send("Title", "message");
+            });
+            t.Wait();
+        }
+    }
+    public interface ICustomNotification
+    {
+        void send(string title, string message);
+    }
+}
